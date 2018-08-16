c96c7afb1bd363739b29db183634f4ce7add1cff
SamKahnke <samkahnke@gmail.com>
2018-08-15 22:42:01 -0500

    add goToStore function onSubmit. Note: browser refresh on form subit. pre-ref/binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,13 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+	goToStore() {
+		alert("went to store");
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please enter a store</h2>
 				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
 				<button type="submit">Visit Store</button>
