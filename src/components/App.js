c2507e6de23e55c1079f85fa7679bfc86a1caeb9
SamKahnke <samkahnke@gmail.com>
2018-08-19 16:58:35 -0500

    add Load Sample Fishes button

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import NotFound from "./NotFound";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
 	state = {
@@ -16,6 +17,10 @@ class App extends React.Component {
 		this.setState({ fishes });
 	}
 
+	loadSampleFishes = () => {
+		this.setState({ fishes: sampleFishes });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -23,7 +28,9 @@ class App extends React.Component {
 					<Header tagline="Fresh Food Daily" />
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory 	addFish={this.addFish}
+							loadSampleFishes={this.loadSampleFishes}
+				/>
 			</div>
 		);
 	}
